5d83dc189f12f09c65a3f16bca2a2bcf1bf0c410
Anderson Arruda <andmarruda@gmail.com>
2022-08-10 17:14:45 -0300

    Update brazil_functions.js

--- brazil_functions.js
+++ brazil_functions.js
@@ -17,3 +17,30 @@ const checkCPF = (cpf) => {
 
     return (sum_dv1 % 11 < 2 ? 0 : 11 - sum_dv1 % 11) == cpf.substr(9,1) && (sum_dv2 % 11 < 2 ? 0 : 11 - sum_dv2 % 11) == cpf.substr(10,1);
 };
+
+/**
+  validates a CNPJ that's brazilian document of companies
+  @version    3.0
+  @author     Anderson Arruda < andmarruda@gmail.com >
+  @param      string cnpj
+  @return     boolean
+**/
+const checkCNPJ = (cnpj) => {
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+    if(cnpj.length != 14 || /^(0{14}|1{14}|2{14}|3{14}|4{14}|5{14}|6{14}|7{14}|8{14}|9{14})$/.test(cnpj))
+        return false;
+    
+    let init_dv1 = 5,
+        init_dv2 = 6,
+        sum_dv1 = cnpj.substr(0,12).split('').reduce((prev, val) => {
+            init_dv1 = init_dv1 == 1 ? 9 : init_dv1;
+            return Number(prev) + (Number(val) * init_dv1--);
+        }, 0),
+
+        sum_dv2 = cnpj.substr(0,13).split('').reduce((prev, val) => {
+            init_dv2 = init_dv2 == 1 ? 9 : init_dv2;
+            return Number(prev) + (Number(val) * init_dv2--);
+        }, 0);
+
+    return (sum_dv1 % 11 < 2 ? 0 : 11 - sum_dv1 % 11) == cnpj.substr(12,1) && (sum_dv2 % 11 < 2 ? 0 : 11 - sum_dv2 % 11) == cnpj.substr(13,1);
+};
